8a19ad2439f9a72ba788bdf09a133e0e925b13c2
RayanaVital <rayanavital@gmail.com>
2025-02-01 23:15:24 -0300

    Add componente de search

---
+++ src/components/Search/index.js
@@ -0,0 +1,11 @@
+function Search() {
+    return (
+        <section>
+            <h2>Já sabe por onde começar ?</h2>
+            <h3>Encontre seu livro na nossa estante.</h3>
+            <input type="text" placeholder="Digite o nome do livro" />
+        </section>
+    );
+}
+
+export default Search;
