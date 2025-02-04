e345ddaff641823fb24ac639344ed8ee6952280d
RayanaVital <rayanavital@gmail.com>
2025-02-04 15:32:08 -0300

    Add books data

---
+++ src/components/Search/searchData.js
@@ -0,0 +1,8 @@
+import livro from '../../images/livro.png';
+
+export const books = [
+    {nome: 'O Senhor dos Anéis', autor: 'J.R.R. Tolkien', ano: 1954, id: 1, src:livro},
+    {nome: 'Harry Potter', autor: 'J.K. Rowling', ano: 1997, id: 2, src:livro},
+    {nome: 'O Pequeno Príncipe', autor: 'Antoine de Saint-Exupéry', ano: 1943, id: 3, src:livro},
+    {nome: 'Dom Quixote', autor: 'Miguel de Cervantes', ano: 1605, id: 4, src:livro},
+]
