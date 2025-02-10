b76ac2b1e3fc8ad47a464768d9de81fb1b4a5ce6
RayanaVital <rayanavital@gmail.com>
2025-02-10 16:21:20 -0300

    Add SuggestionCard to Latest Releases component for recommended books

--- src/components/LatestReleases/index.js
+++ src/components/LatestReleases/index.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { books } from './latestReleasesData';
 import { Title } from '../Title';
+import SugestionCard from "../SugestionCard";
+import bookImagem from '../../images/livro2.png';
 
 const LatestReleasesContainer = styled.section`
     background-color: #EBECEE;
@@ -20,12 +22,19 @@ const NewBooksContainer = styled.div`
 function LatestReleases() {
     return (
         <LatestReleasesContainer>
-            <Title>Últimos Lançamentos</Title>
+            <Title color="#EB9B00" size="36px">Últimos Lançamentos</Title>
             <NewBooksContainer>
                 {books.map(book => (
                     <img src={book.src} alt={book.nome} />
                 ))}
             </NewBooksContainer>
+            <SugestionCard
+                title="Talvez você goste"
+                subtitle="Angular 11"
+                description="Framework para desenvolvimento de aplicações web"
+                img = {bookImagem}
+            
+            ></SugestionCard>
         </LatestReleasesContainer>
     );
 }
