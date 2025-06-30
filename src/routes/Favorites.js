d53648b75dbdc1371b8a77b47fa6e7ed21f39dbd
RayanaVital <rayanavital@gmail.com>
2025-06-30 17:18:19 -0300

    Refactor Favorites component to improve layout and display of favorite books

--- src/routes/Favorites.js
+++ src/routes/Favorites.js
@@ -5,8 +5,44 @@ import { getFavorites } from '../services/favorites';
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(90deg, #002F52 35%, #326589);
+  background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
+
+const ResultContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const Result = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+    cursor: pointer;
+    text-align: center;
+    padding: 0 100px;
+    p {
+        width: 200px;
+        color: #FFF;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
+const Title = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+    padding-top: 35px
+`
+
+
 function Favorites() {
   const [favorites, setFavorites] = useState([]);
 
@@ -21,9 +57,19 @@ function Favorites() {
 
   return (
     <AppContainer>
-      {favorites.map(favorite => (
-        <p>{favorite.title}</p>
-      ))}
+      <div>
+       <Title>Aqui estão seus livros favoritos:</Title>
+       <ResultContainer>
+         {
+           favorites.length !== 0 ? favorites.map(favorite => (
+             <Result>
+               <p>{favorite.title}</p>
+               <img src={favorite.img} alt='imagem do livro'/>
+             </Result>
+           )) : null
+         }
+       </ResultContainer>
+     </div>
     </AppContainer>
   );
 }
